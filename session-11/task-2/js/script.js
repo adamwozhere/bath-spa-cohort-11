a111e7d2ee13456a4cbc9a8a37f7bc3e6d6c5dd4
Adam Wozniak <adamwozhere@gmail.com>
2022-10-27 18:25:19 +0100

    session-11 task-2

--- session-11/task-2/js/script.js
+++ session-11/task-2/js/script.js
@@ -1,25 +1,54 @@
 const shoppingList = document.getElementById('shopping-list');
 const itemInput = document.getElementById('shopping-item');
+const form = document.getElementById('shopping-form');
 
-const shoppingListItems = [];
+const shoppingListItems = ['apples', 'pears', 'stuff'];
 
-function addItem() {
+// the element is automatically added as an argument to addItem function, but why?
+shoppingListItems.forEach(addItem);
+
+console.log(shoppingListItems);
+
+// handler for delete buttons
+form.addEventListener('click', function (e) {
+  // skip if it's not a delete button
+  if (!e.target.classList.contains('delete-btn')) return;
+  // get the li element that has the clicked button
+  let li = e.target.closest('li');
+  // find index of the li in the ul list
+  let index = Array.from(shoppingList.children).indexOf(li);
+  // remove that index from the array
+  shoppingListItems.splice(index, 1);
+  // remove the li from the DOM
+  li.remove();
+});
+
+// handler for adding items
+form.addEventListener('submit', function (e) {
+  // stop form submission
+  e.preventDefault();
+
   const itemToAdd = itemInput.value;
 
-  if (itemToAdd === '') return;
+  if (itemToAdd == '') return;
 
   if (shoppingListItems.includes(itemToAdd)) {
     alert(`You already have ${itemToAdd} on your list!`);
     refocus();
-    return;
+  } else {
+    shoppingListItems.push(itemToAdd);
+    addItem(itemToAdd);
   }
+});
 
-  shoppingListItems.push(itemToAdd);
-  const html = `<li>${itemToAdd} <input type="button" value="x" onclick="alert(this)"></li>`;
+// adds an item to the hml list
+function addItem(itemToAdd) {
+  const html = `<li>${itemToAdd} <button class="delete-btn">x</button</li>`;
   shoppingList.insertAdjacentHTML('beforeend', html);
   refocus();
 }
 
+// clear input
 function refocus() {
   itemInput.value = '';
   itemInput.focus();
