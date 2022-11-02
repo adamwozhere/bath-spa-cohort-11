cf7ff7f82df44288b6cb0eea9b663e2656192935
Adam Wozniak <adamwozhere@gmail.com>
2022-11-02 15:40:34 +0000

    session-12 finish game task
    Tidied up images and odd pages that don't link to anywhere anymore. Added and finished rocket and suit puzzles. Refactored javascript.

--- session-12/js/dragDropPuzzle.js
+++ session-12/js/dragDropPuzzle.js
@@ -1,5 +1,19 @@
+/**
+ * Drag and Drop Puzzles
+ * puzzle pieces need to have and id attribute and a [data-puzzle-piece="rocket"] attribute.
+ * a matching puzzle goal needs to have an id and matching data attribute e.g. [data-puzzle-goal="rocket"]
+ * to show success message, a [data-task] and [data-task-complete] hidden div's are required.
+ *
+ * Note: HTML Drag and Drop API is a bit buggy:  dragOver needs to be cancelled in order from drop to work
+ * and sometimes Chrome browser seems to show ghost image of dragging multiple images,
+ * (or the main .media div) even though pointer events are set to none.
+ * Perhaps using normal click events instead of Drag and Drop API would be better!
+ */
+
+// get puzzle elements
 const puzzlePieces = document.querySelectorAll('[data-puzzle-piece]');
 const puzzleGoals = document.querySelectorAll('[data-puzzle-goal]');
+// successCount used to determine when whole puzzle is finished
 let successCount = puzzleGoals.length;
 
 // add event listeners and classes / attributes
@@ -19,17 +33,19 @@ puzzleGoals.forEach((el) => {
 
 // stop edge cases where media box can be dragged even though no draggable attribute
 document.addEventListener('dragstart', (e) => {
-  console.log('dragging', e.target);
-  if (!e.target.classList.contains('puzzle-draggable')) {
+  // if not valid draggable puzzlepiece then preventDefault()
+  if (!isValidPuzzlePiece(e)) {
     e.preventDefault();
+    return;
   }
+  console.log('dragging', e.target);
 });
 
 // hide puzzle piece while dragging
 function dragStart(e) {
-  console.log('dragging', e.target);
   // exit if not an unplaced puzzle piece
-  if (!e.target.classList.contains('puzzle-draggable')) return;
+  if (!isValidPuzzlePiece(e)) return;
+
   e.dataTransfer.setData('puzzle-piece-id', e.target.id);
   // use delay of zero to stop flicker
   setTimeout(() => {
@@ -44,8 +60,11 @@ function dragEnd(e) {
 
 // hover over puzzle goal
 function dragEnter(e) {
+  // cancel if piece has been placed in goal
   if (e.target.classList.contains('puzzle-placed')) return;
-  if (!e.dataTransfer.types.includes('puzzle-piece-id')) return;
+  // cancel if not a validPuzzlePiece
+  if (!isValidPuzzlePiece(e)) return;
+
   // add hover style
   e.target.classList.add('puzzle-goal-hover');
 }
@@ -65,21 +84,8 @@ function drop(e) {
   // remove hover
   e.target.classList.remove('puzzle-goal-hover');
 
-  // get element id of puzzle piece
-  const id = e.dataTransfer.getData('puzzle-piece-id');
-  // if not a puzzle piece then exit
-  if (id === '') return;
-
-  const puzzlePieceElement = document.getElementById(id);
-  const puzzlePiece = puzzlePieceElement.getAttribute('data-puzzle-piece');
-  const puzzleGoal = e.target.getAttribute('data-puzzle-goal');
-  // correct puzzle piece
-  if (puzzleGoal === puzzlePiece) {
-    e.target.append(puzzlePieceElement);
-    e.target.classList.add('puzzle-placed');
-    puzzlePieceElement.classList.add('puzzle-placed');
-    puzzlePieceElement.classList.remove('puzzle-draggable');
-    puzzlePieceElement.setAttribute('draggable', 'false');
+  if (isMatchingPuzzleEvent(e)) {
+    placePuzzlePieceInGoal(e);
     success();
   }
 }
@@ -90,16 +96,71 @@ function success() {
   if (successCount !== 0) return;
 
   // all puzzle pieces completed
+  // show completed html div / hide task div
   const taskElement = document.querySelector('[data-task]');
   const completeElement = document.querySelector('[data-task-complete]');
-  console.log(completeElement);
-
-  // show completed html
   taskElement.setAttribute('hidden', 'true');
   completeElement.removeAttribute('hidden');
 
-  // set each goal to complete (used for CSS styling)
+  // set each goal to complete (used for CSS styling animations)
   puzzleGoals.forEach((element) => {
+    console.log('puzzle complete!');
     element.classList.add('puzzle-complete');
   });
+}
+
+/**
+ * helper functions
+ */
+
+// checks if the puzzle piece is a valid drag target
+function isValidPuzzlePiece(event) {
+  const piece = event.target;
+  if (
+    piece.hasAttribute('data-puzzle-piece') &&
+    piece.classList.contains('puzzle-draggable') &&
+    !piece.classList.contains('puzzle-placed')
+  ) {
+    return true;
+  } else if (event.dataTransfer.types.includes('puzzle-piece-id')) {
+    return true;
+  }
+
+  return false;
+}
+
+// checks if the event is a correct puzzle piece dropped in the correct goal
+function isMatchingPuzzleEvent(event) {
+  const pieceElement = getPuzzlePieceFromEvent(event);
+  const piece = pieceElement.getAttribute('data-puzzle-piece');
+  const goal = event.target.getAttribute('data-puzzle-goal');
+  console.log('piece', piece, 'goal', goal);
+  if (piece === goal) return true;
+
+  return false;
+}
+
+// gets the puzzle piece being dragged
+function getPuzzlePieceFromEvent(event) {
+  if (!event.dataTransfer.types.includes('puzzle-piece-id')) return null;
+
+  const id = event.dataTransfer.getData('puzzle-piece-id');
+  const element = document.getElementById(id);
+  return element;
+}
+
+// puts the puzzle piece element in the goal
+function placePuzzlePieceInGoal(event) {
+  const piece = getPuzzlePieceFromEvent(event);
+  const goal = event.target;
+  goal.append(piece);
+  goal.classList.add('puzzle-placed');
+  piece.classList.add('puzzle-placed');
+  piece.classList.remove('puzzle-draggable');
+  piece.setAttribute('draggable', 'false');
+
+  if (goal.hasAttribute('data-goal-img')) {
+    const goalImg = event.target.getAttribute('data-goal-img');
+    piece.src = `img/${goalImg}.png`;
+  }
 }
