f17c269aedffd812b4795846a6405cabc0ffa6d8
Adam Wozniak <adamwozhere@gmail.com>
2022-11-12 17:07:54 +0000

    session-15 finished calculator
    some final refactoring may be needed if I can work out how to handle it better.

--- session-15/js/main.js
+++ session-15/js/main.js
@@ -1,291 +1,271 @@
 /**
- * TODO:
- * convert input queue back to being array of each character then combine into operands / operators on calculation in separate step
- * allow negating a minus symbol when pressing plus, and check all the symbol changing rules.
- * display output sting with a space in between each character.
- * handle divide by zero error.
- * show answer on a sperate line.
- * hover styles, backspace button.
+ * CALCULATOR
+ * Has basic error checking / valdation: cannot enter multiply operators in a row, multiple decimals etc.
+ * Has a keyboard handler for using numpad.
+ * Calculates using order of precedence ( division/multiplication then addition/subtraction)
+ * Probably needs much further refactoring, but has become difficult to refactor further at the moment.
  */
 
 const calculator = document.getElementById('calculator');
+const inputDisplay = document.getElementById('input-display');
 const sumDisplay = document.getElementById('sum-display');
 
-let input = ['0'];
-let hasDecimal = false;
-let calculated = false;
-let leadingNegative = false;
-
-const buttonFunctions = {
-  operator: handleOperator,
-  operand: handleOperand,
-  decimal: handleDecimal,
-  calculate,
-  clear,
-};
+let inputQueue = ['0'];
+let previousAction = 'operand';
 
 // button listener
 calculator.addEventListener('click', (e) => {
   const isButton = e.target.nodeName === 'BUTTON'; // button needs to be in caps
   if (!isButton) return;
 
-  const fn = e.target.dataset.function;
-  if (fn !== undefined) {
-    buttonFunctions[fn](e.target.textContent);
+  // animate button
+  animateButtonPress(e.target);
+
+  // the type of action e.g. operator (+, -, /, *)
+  const action = e.target.dataset.action;
+  // the symbol of the action e.g. '-' (minus)
+  const symbol = e.target.innerText;
+  if (action !== undefined) {
+    handleInput(action, symbol);
+    updateDisplay();
   }
 });
 
 // keyboard listener
-document.addEventListener('keyup', (e) => {
+document.addEventListener('keydown', (e) => {
   console.log('key', e.key);
-  switch (e.key) {
-    case '*':
-    case '/':
-    case '-':
-    case '+':
-      handleOperator(e.key);
-      break;
-
-    case '.':
-      handleDecimal();
-      break;
-
-    case 'Delete':
-      clear();
-      break;
-
-    case 'Enter':
-      calculate();
-      break;
-
-    case 'Backspace':
-      backspace();
-      break;
-
-    default:
-      if (Number.isInteger(parseFloat(e.key))) {
-        handleOperand(e.key);
-      }
+  if (e.key === 'Enter') {
+    e.preventDefault(); // stop enter from activating buttons?
+    animateButtonPress(document.getElementById('btn-equals'));
+    handleInput('calculate');
+  } else if (e.key === 'Delete') {
+    animateButtonPress(document.getElementById('btn-clear'));
+    handleInput('clear');
+  } else if (e.key === 'Backspace') {
+    animateButtonPress(document.getElementById('btn-backspace'));
+    handleInput('backspace');
+  } else if (isOperator(e.key)) {
+    animateButtonPress(
+      document.getElementById(`btn-${getOperatorFromSymbol(e.key)}`)
+    );
+    handleInput('operator', e.key);
+  } else if (isOperand(e.key)) {
+    animateButtonPress(document.getElementById(`btn-${e.key}`));
+    handleInput('operand', e.key);
   }
+  updateDisplay();
 });
 
-// input handlers
-function handleOperator(e) {
-  console.log('operator', e);
-  // sum.push(input.join(''));
-  if (isOperator(lastInput())) {
-    console.log('last input is operator', lastInput());
-    input[input.length - 1] = e;
-  } else {
-    if (lastInput() === '0' && e === '-') {
-      input = ['-'];
-      leadingNegative = true;
+// remove focusable for buttons as keyboard navigation not needed
+calculator.addEventListener('focusin', (e) => {
+  if (e.target.nodeName === 'BUTTON') e.target.blur();
+});
+
+/**
+ * Validates an input and formats it into the inputQueue array.
+ * @param {String} action - action type e.g. 'operator'
+ * @param {String} symbol - input symbol e.g. '+'
+ * @returns void
+ */
+function handleInput(action, symbol) {
+  console.log('handleInput', action, symbol, 'prev action', previousAction);
+  if (action === 'clear') {
+    previousAction = 'operand';
+    inputQueue = ['0'];
+    sumDisplay.textContent = '';
+  } else if (action === 'calculate') {
+    handleCalculate();
+  } else if (action === 'backspace') {
+    handleBackspace();
+
+    // HANDLE OPERATORS
+  } else if (action === 'operator') {
+    if (previousAction === 'calculate') {
+      sumDisplay.textContent = inputQueue;
+    }
+    if (inputQueue.length === 1) {
+      if (inputQueue[0] === '0' && symbol === '-') {
+        inputQueue[0] = '-';
+        previousAction = 'operand';
+      } else if (inputQueue[0] === '-' && symbol === '+') {
+        inputQueue[0] = '0';
+        previousAction = 'operand';
+      } else if (isNaN(inputQueue[0])) {
+        console.log('NaN, resetting');
+        inputQueue = ['0'];
+        previousAction = 'operand';
+        sumDisplay.textContent = '';
+      } else {
+        inputQueue.push(symbol);
+        previousAction = 'operator';
+      }
     } else {
-      input.push(e);
+      if (previousAction === 'operand') {
+        inputQueue.push(symbol);
+        previousAction = 'operator';
+      } else if (previousAction === 'operator') {
+        inputQueue[inputQueue.length - 1] = symbol;
+        previousAction = 'operator';
+      }
+    }
+
+    // HANDLE OPERANDS
+  } else if (action === 'operand') {
+    if (symbol === '.' && inputQueue[inputQueue.length - 1].includes('.'))
+      return;
+    if (previousAction === 'calculate') {
+      inputQueue = [symbol];
+      previousAction = 'operand';
+      sumDisplay.textContent = '';
+      return;
+    }
+    if (inputQueue.length === 1) {
+      if (inputQueue[0] === '0') {
+        inputQueue[0] = symbol;
+        previousAction = 'operand';
+      } else {
+        inputQueue[0] += symbol;
+        previousAction = 'operand';
+      }
+    } else {
+      if (previousAction === 'operand') {
+        if (symbol === '0' && inputQueue[inputQueue.length - 1] === '0') return;
+        inputQueue[inputQueue.length - 1] += symbol;
+        previousAction = 'operand';
+      } else if (previousAction === 'operator') {
+        inputQueue.push(symbol);
+        previousAction = 'operand';
+      }
     }
   }
-  hasDecimal = false;
-  console.log('input', input);
-  updateDisplay(e);
-  calculated = false;
-}
-
-function handleOperand(e) {
-  console.log('number', e);
-  if (calculated === true) {
-    input = [e];
-    updateDisplay(e);
-    calculated = false;
-    return;
-  }
-
-  if (isOperator(lastInput())) {
-    if (leadingNegative) {
-      input[input.length - 1] += e;
-      leadingNegative = false;
-    } else {
-      input.push(e);
-    }
-  } else {
-    lastInput() === '0'
-      ? (input[input.length - 1] = e)
-      : (input[input.length - 1] += e);
-  }
-  console.log('operand', input);
-  updateDisplay(e);
-  calculated = false;
-}
-
-function clear(e) {
-  console.log('clear');
-  input = ['0'];
-  hasDecimal = false;
-  updateDisplay(e);
-  calculated = false;
-}
-
-function backspace() {
-  input.pop();
-  if (input.length === 0) {
-    input = ['0'];
-  }
-  updateDisplay();
-  console.log(input);
-  calculated = false;
-}
-
-function handleDecimal() {
-  if (hasDecimal) return;
-
-  if (isOperator(lastInput())) {
-    input.push('.');
-    hasDecimal = true;
-  } else if (input[input.length - 1] === '0') {
-    input[input.length - 1] = '.';
-  } else {
-    input[input.length - 1] += '.';
-    hasDecimal = true;
-  }
-  console.log('decimal', input);
-  updateDisplay();
-  calculated = false;
+  console.log('inputQueue', inputQueue);
 }
 
 // calculate validated sum
-function calculate() {
-  if (isOperator(lastInput()) || calculated) return;
-  console.log('calculate');
+function handleCalculate() {
+  if (
+    inputQueue.length < 3 ||
+    previousAction !== 'operand' ||
+    inputQueue[inputQueue.length - 1] === '.'
+  )
+    return;
 
-  for (let i = 0; i <= input.length; i++) {
-    let sum = 0;
-    console.log(i, input[i]);
-    if (input[i] === '/') {
-      sum = operations['divide'](
-        parseFloat(input[i - 1]),
-        parseFloat(input[i + 1])
+  let sumQueue = inputQueue.join(' ');
+  console.log('calculate', inputQueue);
+
+  // calculate inputQueue left-to-right in order of precedence,
+  // division OR multiplication,
+  // then addition OR subtraction.
+  //
+  // division OR multiplication
+  for (let i = 0; i < inputQueue.length; i++) {
+    let calc = 0;
+    console.log(i, inputQueue[i]);
+    if (inputQueue[i] === '/' || inputQueue[i] === '*') {
+      calc = operate(
+        getOperatorFromSymbol(inputQueue[i]),
+        inputQueue[i - 1],
+        inputQueue[i + 1]
       );
-      input.splice(i - 1, 3, sum);
+      inputQueue.splice(i - 1, 3, calc);
       i -= 1;
-      console.log(sum);
-    } else if (input[i] === '*') {
-      sum = operations['multiply'](
-        parseFloat(input[i - 1]),
-        parseFloat(input[i + 1])
-      );
-      input.splice(i - 1, 3, sum);
-      i -= 1;
-      console.log(sum);
+      console.log(calc);
     }
   }
 
-  for (let i = 0; i <= input.length; i++) {
-    let sum = 0;
-    if (input[i] === '+') {
-      sum = operations['add'](
-        parseFloat(input[i - 1]),
-        parseFloat(input[i + 1])
+  // addition OR subtraction
+  for (let i = 0; i < inputQueue.length; i++) {
+    let calc = 0;
+    if (inputQueue[i] === '+' || inputQueue[i] === '-') {
+      calc = operate(
+        getOperatorFromSymbol(inputQueue[i]),
+        inputQueue[i - 1],
+        inputQueue[i + 1]
       );
-      input.splice(i - 1, 3, sum);
+      inputQueue.splice(i - 1, 3, calc);
       i -= 1;
-      console.log(sum);
-    } else if (input[i] === '-') {
-      sum = operations['subtract'](
-        parseFloat(input[i - 1]),
-        parseFloat(input[i + 1])
-      );
-      input.splice(i - 1, 3, sum);
-      i -= 1;
-      console.log(sum);
+      console.log(calc);
     }
   }
 
-  // let ops = Object.keys(operations);
-  // ops.forEach((fn) => {
-  //   console.log('try operate', fn);
-  //   operate(fn, input);
-  // });
+  // update displays on calculation
+  let equals = parseFloat(inputQueue[0]);
+  sumDisplay.textContent = sumQueue + ' =';
+  inputDisplay.textContent += ' = ' + parseFloat(equals.toFixed(7));
+  previousAction = 'calculate';
+  input = [parseFloat(equals.toFixed(7)).toString()];
 
-  let sum = parseFloat(input[0]);
-  sumDisplay.textContent += '=' + parseFloat(sum.toFixed(7));
-  calculated = true;
-  input = [parseFloat(sum.toFixed(7))];
-
-  console.log('input', input);
+  console.log('input', inputQueue);
 }
 
-function updateDisplay(i) {
-  sumDisplay.textContent = input.join('');
+function updateDisplay() {
+  inputDisplay.textContent = inputQueue.join(' ');
+}
+
+function handleBackspace() {
+  if (previousAction === 'calculate') {
+    inputQueue = ['0'];
+    previousAction = 'operand';
+    sumDisplay.textContent = '';
+    return;
+  }
+  inputQueue[inputQueue.length - 1] = inputQueue[inputQueue.length - 1].slice(
+    0,
+    -1
+  );
+
+  console.log('len', inputQueue.length);
+  if (inputQueue[inputQueue.length - 1].length === 0) inputQueue.pop();
+  if (inputQueue.length === 0) inputQueue = ['0'];
+  if (isOperator(inputQueue[inputQueue.length - 1])) {
+    previousAction = 'operator';
+  } else {
+    previousAction = 'operand';
+  }
+  console.log('backspace', inputQueue);
+}
+
+// operate two numbers together
+// prettier-ignore
+function operate(operator, num1, num2) {
+  const a = parseFloat(num1);
+  const b = parseFloat(num2);
+
+  switch (operator) {
+    case 'divide':    return a / b;
+    case 'multiply':  return a * b;
+    case 'add':       return a + b;
+    case 'subtract':  return a - b;
+  }
 }
 
 /**
- * object containing operation functions
+ *
+ * Helper functions
  */
-const operations = {
-  divide: (a, b) => parseFloat(a / b),
-  multiply: (a, b) => parseFloat(a * b),
-  add: (a, b) => parseFloat(a + b),
-  subtract: (a, b) => parseFloat(a - b),
-};
+// check input types
+const isOperand = (n) => n === '.' || Number.isInteger(parseFloat(n));
+const isOperator = (n) => n === '/' || n === '*' || n === '-' || n === '+';
 
-// operate in sequence BODMAS
-// let ops = Object.keys(operations);
-// ops.forEach((fn) => {
-//   operate(fn, testArray);
-// });
-
-// need to change order of operations as it should be division OR multiplication left to right
-// then addition OR subtraction left to right!
-
-// let total = parseFloat(testArray[0].toFixed(7));
-// console.log(total);
-
-const lastInput = () => input[input.length - 1];
-
-const isOperator = (input) => {
-  switch (input) {
-    case '+':
-    case '-':
+const getOperatorFromSymbol = (s) => {
+  switch (s) {
     case '/':
+      return 'divide';
     case '*':
-      return true;
-
-    default:
-      return false;
+      return 'multiply';
+    case '-':
+      return 'subtract';
+    case '+':
+      return 'add';
   }
 };
 
-/**
- * Calculate an input array with an operator function e.g. 'multiply'
- * @param {string} operator - string matching an operation function name
- * @param {Array} array - array of validated input values
- */
-function operate(operator, array) {
-  for (let i = 0; i <= array.length; i++) {
-    if (array[i] === getSymbolFromOperator(operator)) {
-      console.log('operating', array[i - 1], operator, array[i + 1]);
-      let sum = operations[operator](
-        parseFloat(array[i - 1]),
-        parseFloat(array[i + 1])
-      );
-      array.splice(i - 1, 3, sum);
-      i -= 1;
-      console.log(sum);
-    }
-  }
+// reset and animate button press
+function animateButtonPress(b) {
+  console.log(b);
+  b.classList.remove('btn-pressed');
+  b.classList.add('btn-pressed');
+  setTimeout(() => {
+    b.classList.remove('btn-pressed');
+  }, 100);
 }
-
-const getSymbolFromOperator = (o) => {
-  switch (o) {
-    case 'add':
-      return '+';
-
-    case 'subtract':
-      return '-';
-
-    case 'divide':
-      return '/';
-
-    case 'multiply':
-      return '*';
-  }
-};
